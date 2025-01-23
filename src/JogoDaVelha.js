c7a0f81d2f4fd2e414b491ade3f6f3590d8e3abf
Mario Torres <scaramuzzini@gmail.com>
2025-01-23 09:41:47 -0500

    Quadrado adicionado

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -1,28 +1,38 @@
 import './Jogo.css'
 
+
 export default function JogoDaVelha() {
     return <>
         <p>Este é o jogo da velha</p>
         <Tabuleiro />
     </>
 }
+import { useState } from 'react'
+function Quadrado() {
+    const [valor, setValor] = useState('')
+
+    function handleClick() {
+        setValor('X')
+    }
+    return <button className="q" onClick={handleClick}>{valor}</button>
+}
 
 function Tabuleiro() {
     return <>
     <div className="linha">
-        <button className="q">X</button>
-        <button className="q">X</button>
-        <button className="q">X</button>
+        <Quadrado/>
+        <Quadrado/>
+        <Quadrado/>
     </div>
     <div className="linha">
-        <button className="q">X</button>
-        <button className="q">X</button>
-        <button className="q">X</button>
+        <Quadrado/>
+        <Quadrado/>
+        <Quadrado/>
     </div>
     <div className="linha">
-        <button className="q">X</button>
-        <button className="q">X</button>
-        <button className="q">X</button>
+        <Quadrado/>
+        <Quadrado/>
+        <Quadrado/>
     </div>
     </>
 }
