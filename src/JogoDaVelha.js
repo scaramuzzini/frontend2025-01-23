4bc7134099e22b57c495865b5fc281c210b182e0
Mario Torres <scaramuzzini@gmail.com>
2025-01-23 10:14:41 -0500

    Array de quadraos

--- src/JogoDaVelha.js
+++ src/JogoDaVelha.js
@@ -1,4 +1,5 @@
 import './Jogo.css'
+import { useState } from 'react'
 
 
 export default function JogoDaVelha() {
@@ -7,32 +8,45 @@ export default function JogoDaVelha() {
         <Tabuleiro />
     </>
 }
-import { useState } from 'react'
-function Quadrado() {
-    const [valor, setValor] = useState('')
-
-    function handleClick() {
-        setValor('X')
-    }
-    return <button className="q" onClick={handleClick}>{valor}</button>
+function Quadrado({valor}) {
+    return <button className="q">{valor}</button>
 }
 
 function Tabuleiro() {
+
+    const [quadrados,setQuadrados] = useState(Array(9).fill(''))
+    const [vezDoX, setVezDoX] = useState(true)
+    let msg = '';
+    if (vezDoX) {
+        msg = 'Vez do jogador X';
+    } else {
+        msg = 'Vez do jogador O';
+    }
+
+    function handleClick() {
+        if (vezDoX) {
+            //setValor('X')
+        } else {
+            //setValor('O')
+        }
+    }
+
     return <>
+    <p>{msg}</p>
     <div className="linha">
-        <Quadrado/>
-        <Quadrado/>
-        <Quadrado/>
+        <Quadrado valor={quadrados[0]}/>
+        <Quadrado valor={quadrados[1]}/>
+        <Quadrado valor={quadrados[2]}/>
     </div>
     <div className="linha">
-        <Quadrado/>
-        <Quadrado/>
-        <Quadrado/>
+        <Quadrado valor={quadrados[3]}/>
+        <Quadrado valor={quadrados[4]}/>
+        <Quadrado valor={quadrados[5]}/>
     </div>
     <div className="linha">
-        <Quadrado/>
-        <Quadrado/>
-        <Quadrado/>
+        <Quadrado valor={quadrados[6]}/>
+        <Quadrado valor={quadrados[7]}/>
+        <Quadrado valor={quadrados[8]}/>
     </div>
     </>
 }
